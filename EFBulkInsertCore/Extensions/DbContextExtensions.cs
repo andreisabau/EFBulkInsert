c46bd65a89b1c41ac7b2363bb74483f89cf2febb
Andrei Sabau <hello@andreisabau.com>
2020-03-08 06:23:16 +0200

    Add efcore version

---
+++ EFBulkInsertCore/Extensions/DbContextExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using EFBulkInsertCore.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EFBulkInsertCore.Extensions
+{
+    internal static class DbContextExtensions
+    {
+        public static SqlConnection GetSqlConnection(this DbContext dbContext)
+        {
+            SqlConnection sqlConnection = (SqlConnection) dbContext.Database.GetDbConnection();
+
+            return sqlConnection;
+        }
+
+        public static SqlTransaction GetSqlTransaction(this DbContext dbContext)
+        {
+            SqlTransaction sqlTransaction = dbContext.Database.CurrentTransaction?.GetDbTransaction() as SqlTransaction;
+
+            return sqlTransaction;
+        }
+
+        public static EntityMetadata GetEntityMetadata<T>(this DbContext dbContext)
+        {
+            IEntityType entityType = dbContext.Model.FindEntityType(typeof(T));
+
+            EntityMetadata entityMetadata = new EntityMetadata
+            {
+                TempTableName = "##TEMP_" + Guid.NewGuid().ToString().Replace('-', '_'),
+                Type = typeof(T),
+                TableName = entityType.GetTableName(),
+                Properties = entityType.GetProperties().Select(x => new EntityProperty
+                {
+                    ColumnName = x.GetColumnName(),
+                    IsDbGenerated = x.ValueGenerated == ValueGenerated.OnAddOrUpdate || x.ValueGenerated == ValueGenerated.OnAdd,
+                    SqlServerType = GetSqlServerType(x),
+                    IsNullable = x.IsNullable,
+                    PropertyName = x.Name
+                }).ToList()
+            };
+
+            return entityMetadata;
+        }
+
+        private static string GetSqlServerType(IProperty property)
+        {
+            string type = property.GetColumnType();
+
+            if (property.IsNullable)
+            {
+                type = $"{type} NULL";
+            }
+
+            return type;
+        }
+    }
+}
---
+++ EFBulkInsertCore/Models/EntityProperty.cs
@@ -0,0 +1,11 @@
+namespace EFBulkInsertCore.Models
+{
+    internal class EntityProperty
+    {
+        public string ColumnName { get; set; }
+        public bool IsDbGenerated { get; set; }
+        public string SqlServerType { get; set; }
+        public bool IsNullable { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
